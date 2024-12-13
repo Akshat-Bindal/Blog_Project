52504f09f5dfcda2d0b0474e4c76f44e347718a7
Akshat-Bindal <biakshat31@gmail.com>
2024-12-14 02:09:05 +0530

    integrated redux

--- Blog_project/src/App.jsx
+++ Blog_project/src/App.jsx
@@ -1,13 +1,41 @@
+import { useState,useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth.js'
+import {login,logout} from './store/authSlice.js'
 import './App.css'
+import { Header,Footer } from './components/index.js'
+import {Outlet} from 'react-router-dom'
 
 function App() {
 
+  const [loading,setloading]=useState()
+  const dispatch=useDispatch()
 
-  return (
-    <>
-      <div>Blog_App</div>
-    </>
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    }
   )
+  .finally(()=>setloading(false))
+  },[])
+
+
+  return !loading ?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+      </div>
+    </div>
+  ):null
+
 }
 
 export default App
